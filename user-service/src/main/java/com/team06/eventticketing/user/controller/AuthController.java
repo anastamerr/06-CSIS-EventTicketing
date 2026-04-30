4d27ee4ff8624248126f96b1298be12646ae14a6
anastamerr <anastamerr@outlook.com>
2026-04-30 19:00:25 +0300

    fix(user): keep S1-F11 compose-safe defaults (55-12331)

--- user-service/src/main/java/com/team06/eventticketing/user/controller/AuthController.java
+++ user-service/src/main/java/com/team06/eventticketing/user/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.team06.eventticketing.user.controller;
 
 import com.team06.eventticketing.user.dto.AuthResponse;
+import com.team06.eventticketing.user.dto.LoginRequest;
 import com.team06.eventticketing.user.dto.RegisterRequest;
 import com.team06.eventticketing.user.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import com.team06.eventticketing.user.dto.LoginRequest;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -25,6 +26,7 @@ public class AuthController {
     public AuthResponse register(@RequestBody RegisterRequest request) {
         return userService.register(request);
     }
+
     @PostMapping("/login")
     public AuthResponse login(@RequestBody LoginRequest request) {
         return userService.login(request);
